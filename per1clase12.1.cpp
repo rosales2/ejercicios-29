2933615feced18da15b9e270ae16a43875fa3186
rosales2 <112508442+rosales2@users.noreply.github.com>
2022-09-06 12:10:55 -0600

    Add files via upload

---
+++ per1clase12.1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	float limite, contador;
+	cout << "Ingreseel limite ";
+	cin >> limite;
+		while (contador < limite){
+		contador = contador + 1;
+		cout << "Contador " << contador << endl;
+	}
+}
